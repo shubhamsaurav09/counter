6ceb92b9021e7fcb9e84cdb78c9aa132b1387f96
shubhamsaurav09 <thebeststrikertorres@gmail.com>
2022-10-06 04:45:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const disableIncrement = count === 10 ? true : false;
+  const disableDecrement = count === 0 ? true : false;
+
+  const handleIncrement = ({ EventTarget }) => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = ({ EventTarget }) => {
+    setCount((prev) => prev - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Counter App</h1>
+      <div>
+        <h2>{count}</h2>
+        <button
+          id="increment"
+          onClick={handleIncrement}
+          disabled={disableIncrement}
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <button
+          id="decrement"
+          onClick={handleDecrement}
+          disabled={disableDecrement}
+        >
+          Decrement
+        </button>
+        <button
+          id="reset"
+          onClick={() => setCount(0)}
+          disabled={disableDecrement}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
